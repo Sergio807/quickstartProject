6254020b261d022994c5da310436270b5e86fc08
xorse <s.vladimirowi420@yandex.ru>
2022-01-31 22:00:03 +0300

    quick1.5

--- src/main/java/com/geekbrains/lesson3/GiftShop.java
+++ src/main/java/com/geekbrains/lesson3/GiftShop.java
@@ -15,13 +15,16 @@ public class GiftShop {
         driver.get("https://vpodarkov.ru/test_auth.php");
         //Thread.sleep(5000);
         new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.findElement(By.xpath("//a[.='Каталог']")).click();
-        driver.findElement(By.xpath("//img[@alt='Живопись']")).click();
-        driver.findElement(By.xpath("//a[@href='/catalog/328/?action=ADD2BASKET&id=3775']")).click();
+        //driver.findElement(By.xpath("//a[.='Каталог']")).click();
+        //driver.findElement(By.xpath("//img[@alt='Живопись']")).click();
+        //driver.findElement(By.xpath("//a[@href='/catalog/328/?action=ADD2BASKET&id=3775']")).click();
         new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.findElement(By.xpath("//ancestor::div[@class='label']//input[@name='USER_LOGIN']")).sendKeys("QAQ");
         driver.findElement(By.xpath("//ancestor::div[@class='label']//input[@name='USER_PASSWORD']")).sendKeys("Test60test");
         driver.findElement(By.xpath("//ancestor::div[@class='btn']/child::input[@class='btn-send btn-line']")).click();
+        driver.findElement(By.xpath("//span[@class='name']")).click();
+        //driver.findElement(By.xpath("//ancestor::div[@class='topCart']/child::a[@href='/personal/cart/']"));
+        driver.findElement(By.xpath("//span[@class='delete']")).click();
        //В коде страницы есть по 2 значения полей логин и пароль и 2 значения кнопки "Войти".Пришлось немного подумать
         Thread.sleep(10000);
         driver.quit();
---
+++ src/test/java/com/geekbrains/lesson5/GiftShopTest.java
@@ -0,0 +1,65 @@
+package com.geekbrains.lesson5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.lang.model.element.Element;
+import java.time.Duration;
+
+public class GiftShopTest {
+    WebDriver driver;
+    WebDriverWait webDriverWait;
+    Actions actions;
+    private final static String GIFTSHOP_URL = "https://vpodarkov.ru/";
+
+    //Регистрируем драйвер
+    @BeforeAll
+    static void registerDriver(){
+        WebDriverManager.chromedriver().setup();
+    }
+    //Инициализируем драйвер под те действия/команды, которые будем с ним выполнять
+    @BeforeEach
+    void initDriver(){
+        driver = new ChromeDriver();
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        actions = new Actions(driver);
+        driver.get(GIFTSHOP_URL);
+        driver.manage().window().maximize();
+    }
+    @Test
+    void giftCookieTest() throws InterruptedException {
+        Cookie cookie = new Cookie("PHPSESSID", "18ec14c55db3073db138499c33c1c209");
+        driver.manage().addCookie(cookie);
+        driver.navigate().refresh();
+        //WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(8));
+        Thread.sleep(10000);
+    }
+
+    //Очень трудно что-либо делать не понимая где, как и какой метод можно использовать
+    //Хотел проверить возможность удаления элемента с помощью теста, но не знаю какой метод assertion использовать
+    //@Test
+    //void giftClearCart(){
+        //driver.findElement(By.xpath("//span[@class='delete']")).click();
+    //}
+
+    @Test
+    void giftBannerTest(){
+        driver.findElement(By.xpath("//a[@title='ООО «Время подарков» - интернет магазин эксклюзивных сувениров и подарков.']")).click();
+        Assertions.assertTrue(driver.findElement(By.xpath("//a[@title='ООО «Время подарков» - интернет магазин эксклюзивных сувениров и подарков.']")).isDisplayed());
+        new WebDriverWait(driver, Duration.ofSeconds(8));
+    }
+
+
+
+
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+    }
+}
